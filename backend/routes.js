64466d11c4360846cd7be318afb44e9bd4321dac
Kaveen Abeywansa <kaveenabeywansa@gmail.com>
2019-05-30 23:04:50 +0530

    backend - added user route to main navigation

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 var Routes = express.Router();
 
+const UserRoutes = require('./route/user-route');
+
+Routes.use('/users/', UserRoutes);
 Routes.use('/', function(req, res) {
     res.sendFile(__dirname+'/welcome.html');
 });
